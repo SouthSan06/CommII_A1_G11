beeff65aa67ae24dc38158d8ecd859d2bffdfe42
ESTUDIANTE <andreslepu12@gmail.com>
2026-03-06 07:45:26 -0500

    actualizacion de evidencia

--- GNURadio/prac3a.py
+++ GNURadio/prac3a.py
@@ -68,7 +68,7 @@ class prac3a(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.h = h = np.array([1,1,1,1])
+        self.h = h = np.array([1,1,1,1,1,1,1,1])
         self.Sps = Sps = len(h)
         self.Rb = Rb = 32000
         self.samp_rate = samp_rate = Rb*Sps
@@ -242,7 +242,7 @@ class prac3a(gr.top_block, Qt.QWidget):
         self.blocks_null_sink_0 = blocks.null_sink(gr.sizeof_float*1)
         self.blocks_multiply_const_vxx_1 = blocks.multiply_const_vff([1/(N*samp_rate)]*N)
         self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff(2.)
-        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_char*1, '/home/estudiante/Descargas/rana.jpg', True, 0, 0)
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_char*1, '/home/estudiante/commll_A1_G11/CommII_A1_G11/GNURadio/rana.jpg', True, 0, 0)
         self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
         self.blocks_complex_to_mag_squared_0 = blocks.complex_to_mag_squared(N)
         self.blocks_char_to_float_0 = blocks.char_to_float(1, 1)
